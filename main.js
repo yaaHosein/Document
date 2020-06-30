bb09538e702072a3ac2af760dbd95038ed4f598d
yaaHosein <layali.14.peshawar@hotmail.com>
2020-06-30 16:24:06 +0430

    document js

---
+++ main.js
@@ -0,0 +1,11 @@
+const element1 = document.getElementById("main1");
+console.log(element1);
+element1.textContent = "Div one"
+
+const element2 = document.getElementById("main2");
+console.log(element2);
+element2.textContent = "Div two"
+
+const element3 = document.getElementById("main3");
+console.log(element3);
+element3.textContent = "Div three"
